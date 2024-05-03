1092df2fab1ecfb6aa35605c9ac7a8e28780aae9
haneya-studio <jjakahane@gmail.com>
2024-05-03 22:32:29 +0900

    add search

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,8 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { 
+  LinksFunction,
+  LoaderFunctionArgs,
+} from "@remix-run/node";
+
 import { json, redirect } from "@remix-run/node";
 import {
   Form,
@@ -20,8 +24,12 @@ export const links: LinksFunction = () => [
 
 import { createEmptyContact, getContacts } from "./data";
 
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 export const action = async () => {
