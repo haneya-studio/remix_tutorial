c1d6a906302212e109a444dd61b3535dbd4213b0
haneya-studio <jjakahane@gmail.com>
2024-05-04 09:06:16 +0900

    add 404 to dynamic routing

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,14 +1,22 @@
 import React from "react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 import type { ContactRecord } from "../data";
 import { getContact } from "../data";
+import invariant from "tiny-invariant";
 
-export const loader = async ({ params }) => {
-  const contact = await getContact(params.contactId);
-  return json({ contact });
-}
+export const loader = async ({
+    params,
+  }: LoaderFunctionArgs) => {
+    invariant(params.contactId, "Missing contactId param");
+    const contact = await getContact(params.contactId);
+    if (!contact) {
+      throw new Response("Not Found", { status: 404 });
+    }    
+    return json({ contact });
+  };
 
 ;export default function Contact() {
   const { contact } = useLoaderData<typeof loader>();
